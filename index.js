69dc08e4b8397c219e93944e5c911e3a2af7223a
Criticalcarpet <76092002+Criticalcarpet@users.noreply.github.com>
2022-03-07 07:44:26 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
-import  { Client, Intents } from 'discord.js';
-import 'dotenv/config'
-const token = process.env.TOKEN
-
-// Create a new client instance
+import { Client, Intents } from "discord.js";
+import "dotenv/config";
+import { pingMonitor } from "./lib/ping.js";
+const token = process.env.TOKEN;
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-// When the client is ready, run this code (only once)
-client.once('ready', () => {
-	console.log('Ready!');
+client.once("ready", async () => {
+  console.log("Ready!");
+  setInterval(async () => {
+    const response = await pingMonitor();
+    if (response.monitors[0].status === 2) {
+      // change it with the server status channel
+      if (
+        client.channels.cache.get("926071148872093726").name !==
+        "🟢-kekbot-status"
+      ) {
+        client.channels.cache
+          .get("926071148872093726")
+          .setName("🟢-kekbot-status");
+      }
+    } else {
+      if (
+        client.channels.cache.get("926071148872093726").name !==
+        "🔴-kekbot-status"
+      ) {
+        client.channels.cache
+          .get("926071148872093726")
+          .setName("🔴-kekbot-status");
+      }
+    }
+  }, 5 * 60 * 1000);
 });
-
-// Login to Discord with your client's token
 client.login(token);
